da9c16bf10a7ffedf8b9f8df00262abd65b9f14b
ahmedashin999 <ahmedashin999@gmail.com>
2020-12-19 17:28:36 +0600

    deploy to heroku

---
+++ app.js
@@ -0,0 +1,89 @@
+const express =require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db.js')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const path = require('path')
+const passport = require('passport')
+const session = require('express-session')
+const mongoose = require('mongoose')
+const MongoStore = require('connect-mongo')(session)
+
+//Load config
+
+dotenv.config({path:'./config/config.env'})
+
+//passport config
+
+require('./config/passport')(passport)
+
+connectDB()
+
+const app = express();
+
+//Body parser
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+///Method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
+
+//Logging
+if(process.env.NODE_ENV === 'development') {
+     app.use(morgan('dev'))
+}
+
+//handlebars helpers
+const { formateDate,stripTags,truncate,editIcon,select} = require('./helpers/hbs')
+ 
+
+//handlebars
+
+app.engine('.hbs',exphbs({helpers:{
+    formateDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select
+
+}, defaultLayout:'main', extname:'.hbs'}));
+app.set('view engine','.hbs');
+
+//Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave:false,
+    saveUninitialized:false,
+    store:new MongoStore({ mongooseConnection: mongoose.connection})
+}))
+
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session())
+
+//set global variables
+app.use(function(req,res,next) {
+    res.locals.user = req.user || null
+    next()
+})
+
+//Static
+app.use(express.static(path.join(__dirname,'public')))
+
+//Routes
+app.use('/',require('./routes/index'))
+app.use('/auth',require('./routes/auth'))
+app.use('/stories',require('./routes/stories'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT ,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
